de48a645334278e10f09072b3717685f1d70f63b
take917 <1tktk2tk3@gmail.com>
2022-05-22 22:11:45 +0900

    ✨ makeConponentsLayoutAndButton

---
+++ pages/button.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Layout } from '../components/Layout'
+
+const ButtonDemo = () => {
+  return <Layout title="Button">button</Layout>
+}
+
+export default ButtonDemo
