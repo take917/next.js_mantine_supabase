c4cf9d47411f6013bd83fb8542f78fcf15957d8a
take917 <1tktk2tk3@gmail.com>
2022-05-23 22:48:57 +0900

    🎨 setting mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGetInitialProps } from '@mantine/next'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { MantineProvider } from '@mantine/core'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+          fontFamily: 'Verdana,sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+        <Component {...pageProps} />
+      </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
