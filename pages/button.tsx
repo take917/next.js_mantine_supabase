0ae39c126369d75e67b3495934c9d83bd8267338
take917 <1tktk2tk3@gmail.com>
2022-05-23 22:50:28 +0900

    ⚡️ make a button

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Button, Group } from '@mantine/core'
 import { Layout } from '../components/Layout'
+import { BrandGithub, ThreeDCubeSphere } from 'tabler-icons-react'
 
 const ButtonDemo = () => {
-  return <Layout title="Button">button</Layout>
+  return (
+    <Layout title="Button">
+      <Group direction="column" position="center">
+        <Button
+          classNames={{
+            leftIcon: 'text-pink-500 h-6 w-6',
+            rightIcon: 'text-orange-500 h-6 w-6',
+          }}
+          color="cyan"
+          radius="lg"
+          uppercase
+          leftIcon={<BrandGithub />}
+          rightIcon={<ThreeDCubeSphere />}
+        >
+          Press
+        </Button>
+        <Button mt="md">Click</Button>
+      </Group>
+    </Layout>
+  )
 }
 
 export default ButtonDemo
